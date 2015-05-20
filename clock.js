9eca7e8e01cf786763ea8493c017d65d15612898
PhineasClick <Phineas.Click@googlemail.com>
2015-05-20 18:45:26 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -12,14 +12,17 @@
 		
 	}
 
+	function getRandomInt(min, max) {
+        	return Math.floor(Math.random() * (max - min + 1)) + min;
+    	}
 
 	this.clickDownOnEntity = function(entityID, mouseEvent) { 
         print("clickDownOnEntity()...");
+        	
+        	Entities.editEntity(myID, { color: { red: getRandomInt(128,255), green: getRandomInt(128,255), blue: getRandomInt(128,255)} });
         
         
-        
-        
-    }; 
+    	}; 
 
 
 
