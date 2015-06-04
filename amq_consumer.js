4ebfbd349bb5695d5794c4a25fcc10b71823add3
PhineasClick <Phineas.Click@googlemail.com>
2015-06-04 12:43:32 +0200

    fix

--- amq_consumer.js
+++ amq_consumer.js
@@ -4,8 +4,9 @@ AmqAConsumer =  function{
 Script.include("amq.js");
 
 var amq = Amq;
+
   amq.init({ 
-    uri: http://localhost:61616/;, 
+    uri: "http://localhost:61616/", 
     logging: true,
     timeout: 20
   });
