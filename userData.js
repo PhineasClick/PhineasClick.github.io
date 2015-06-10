c94117e869665e852a826fee44156c7ac3058d06
PhineasClick <Phineas.Click@googlemail.com>
2015-06-10 21:31:53 +0200

    test

--- userData.js
+++ userData.js
@@ -1,10 +1,14 @@
 
 (function(){
     this.clickReleaseOnEntity = function(entityID, mouseEvent) { 
+    	print("event entityID : " + entityID);
+    	print("this entityID : " + this.entityID);
         if (this.entityID === null) {
             this.entityID = entityID;
         }
+        
         var properties = Entities.getEntityProperties(this.entityID);
+        
         print("Entity Properties : " + JSON.stringify(properties));
         print("UserData : " + properties.userData);    
     };
